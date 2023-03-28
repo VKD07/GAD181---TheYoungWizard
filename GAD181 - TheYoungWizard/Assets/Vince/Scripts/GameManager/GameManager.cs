81a58a28554df9abbc8e72b35c0f23b53d4bb3e2
VinceKyleDelgado <v.delgado@student.saedubai.com>
2023-03-28 07:17:52 +0400

    Room Scene: Added Map UI

---
+++ GAD181 - TheYoungWizard/Assets/Vince/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+
+}
