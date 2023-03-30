848e3326ee8c736e0aa71acebee49c66884ade9b
SaifAl75 <117916115+SaifAl75@users.noreply.github.com>
2023-03-31 03:48:31 +0400

    Winter Land - Death Animation & Bug Enemy Completed

--- GAD181 - TheYoungWizard/Assets/Saif/Script/WinterLandEnemyAI.cs
+++ GAD181 - TheYoungWizard/Assets/Saif/Script/WinterLandEnemyAI.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class WinterLandEnemyAI : MonoBehaviour
 {
     [SerializeField] public NavMeshAgent agent;
+    [SerializeField] float currentHealth;
+    [SerializeField] Slider slider;
+    float maxHealth;
     public GameObject thatPlayer;
+    public float attackDamage;
     public Transform player;
     public LayerMask thePlayer;
     public LayerMask theGround;
@@ -21,46 +26,72 @@ public class WinterLandEnemyAI : MonoBehaviour
         player = GameObject.Find("Player(In-Game)").transform;
         agent = GetComponent<NavMeshAgent>();
         animEnemy = GetComponent<Animator>();
-        thatPlayer.GetComponent<playerCombat>();
+        slider.maxValue = currentHealth;
+        maxHealth = currentHealth;
+
+    }
+    public void DamageEnemy(float playerDamage)
+    {
+        currentHealth -= playerDamage;
     }
     private void EnemyChase()
     {
+        
         agent.SetDestination(player.position);
         transform.LookAt(player);
+        animEnemy.SetBool("Run Forward", true);
+    }
+    private void EnemyDeath()
+    {
+        agent.SetDestination(transform.position);
+        animEnemy.SetBool("Death",true);
+        Destroy(gameObject, 3);
+
 
     }
+
     private void EnemyAttack()
     {
         agent.SetDestination(transform.position);
         transform.LookAt(player);
-        thatPlayer.GetComponent<playerCombat>().damagePlayer(10f);
+        
+        animEnemy.SetTrigger("Attack1");
+        animEnemy.SetTrigger("Attack2");
+        animEnemy.SetBool("Run Forward", false);
+    }
+
+    private void EnemyDealDamage()
+    {
+        thatPlayer.GetComponent<playerCombat>().damagePlayer(attackDamage);
+    }
+    private void UpdateEnemyHealth()
+    {
+        slider.value = currentHealth;
     }
 
 
-  
 
     private void Update()
     {
         playerSightRange = Physics.CheckSphere(transform.position, sightRange, thePlayer);
         playerAttackRange = Physics.CheckSphere(transform.position, attackRange, thePlayer);
+        UpdateEnemyHealth();
 
 
-        if (playerSightRange && !playerAttackRange)
-        {
-            EnemyChase();
-            animEnemy.SetBool("Run Forward", true);
-
+        if (playerSightRange && !playerAttackRange || currentHealth < maxHealth)
+        {   
+            EnemyChase();       
         }
+
         if (playerSightRange && playerAttackRange)
         {
-            animEnemy.SetTrigger("Attack1");
-            animEnemy.SetTrigger("Attack2");                       
-            animEnemy.SetBool("Run Forward", false);
+            EnemyAttack();
         }
 
-        if (!playerSightRange && !playerAttackRange)
+
+        if (currentHealth <= 0)
         {
-            animEnemy.SetBool("Run Forward", false);
+            EnemyDeath();
         }
 
     }
