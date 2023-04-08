83bf66e6f616d07d601afd1b02ec9a4a04ab3943
SaifAl75 <117916115+SaifAl75@users.noreply.github.com>
2023-04-08 20:56:54 +0400

    Minor Changes to the Script

--- GAD181 - TheYoungWizard/Assets/Vince/Scripts/Player(In-Game Scripts)/playerCombat.cs
+++ GAD181 - TheYoungWizard/Assets/Vince/Scripts/Player(In-Game Scripts)/playerCombat.cs
@@ -75,8 +75,8 @@ public class playerCombat : MonoBehaviour
     [SerializeField] ParticleSystem healParticles;
     [SerializeField] ParticleSystem manaParticles;
 
-    [Header("Respawn Point")]
-     public Vector3 spawnPoint;
+    //[Header("Respawn Point")]
+    //public Vector3 spawnPoint;
 
     public bool dodge = false;
     public float shieldDuration = 3f;
@@ -96,7 +96,7 @@ public class playerCombat : MonoBehaviour
         cam.m_Lens.FieldOfView = 33f;
         midRig.m_TrackedObjectOffset.x = 0.25f;
         playerMovement = GetComponent<Player_Movement>();
-        spawnPoint = transform.position;
+        //spawnPoint = transform.position;
     }
 
     private void CursorLoc()
@@ -133,19 +133,19 @@ public class playerCombat : MonoBehaviour
         SpellCastAnimation();
         DisablingDamageIndicator();
         DeathHandler();
-        if (Input.GetKeyDown(KeyCode.M))
+       /* if (Input.GetKeyDown(KeyCode.M))
         {
             transform.position = spawnPoint;
-        }
+        }*/
     }
 
     private void DeathHandler()
     {
         if (playerHealth <= 0 && !tutorial)
         {
-           // SceneManager.LoadScene("RoomScene");
-           transform.position = spawnPoint;
-           playerHealth = 100;
+           SceneManager.LoadScene("RoomScene");
+           //transform.position = spawnPoint;
+          //playerHealth = 100;
             
         }
     }
@@ -506,10 +506,10 @@ public class playerCombat : MonoBehaviour
             cam.m_Lens.FieldOfView += 80f * Time.deltaTime;
         }
     }
-    public void RespawnPoint(Vector3 point)
+   /*public void RespawnPoint(Vector3 point)
     {
         spawnPoint = point;
-    }
+    }*/
 
     }
 
