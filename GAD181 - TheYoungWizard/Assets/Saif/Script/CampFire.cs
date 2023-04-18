336c209a5aaa4375b4780b1e4b8b6d6fcc88f5b3
SaifAl75 <117916115+SaifAl75@users.noreply.github.com>
2023-04-18 10:02:48 +0400

    Winter Land - Campfire Update & New Skybox

--- GAD181 - TheYoungWizard/Assets/Saif/Script/CampFire.cs
+++ GAD181 - TheYoungWizard/Assets/Saif/Script/CampFire.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CampFire : MonoBehaviour
 {
     [SerializeField] ParticleSystem campFire;
-
-
+    [SerializeField] GameObject fireSpanwer;
+    public Vector3 firePlacement;
     
    public void PlayFire()
    {
         campFire.Play();
    }
-        
+
+
+    private void Start()
+    {
+        firePlacement = new Vector3(fireSpanwer.transform.position.x, fireSpanwer.transform.position.y, fireSpanwer.transform.position.z);
+ 
+    }
+
 }
---
+++ GAD181 - TheYoungWizard/Assets/Saif/Script/CharacterSprintEffect.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSprintEffect : MonoBehaviour
+{
+    [SerializeField] ParticleSystem fastStep;
+
+
+
+    public void PlayFastSteps()
+    {
+
+            fastStep.Play();
+
+        
+    }
+
+
+
+}
